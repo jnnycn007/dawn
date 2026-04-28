c89b9992aeb930e68351277c6547dd114ac8e9ac
Kai Ninomiya <kainino@chromium.org>
2026-04-28 12:41:59 -0700

    [dawn][common] Implement checked_cast for strong types
    We want to use checked_cast with ityp:: container index types. This
    makes it easier. This will also come in handy in the future as we add
    more checked_casts.
    
    Bug: 442860471, 503403892
    Change-Id: I223900ca9790bc85211f420e4f2536abf5be5edf
    Reviewed-on: https://dawn-review.googlesource.com/c/dawn/+/305555
    Reviewed-by: Antonio Maiorano <amaiorano@google.com>
    Commit-Queue: Kai Ninomiya <kainino@chromium.org>

--- src/dawn/tests/unittests/NumericTests.cpp
+++ src/dawn/tests/unittests/NumericTests.cpp
@@ -25,14 +25,17 @@
 // OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+#include <limits>
+
 #include "dawn/common/Numeric.h"
+#include "dawn/common/TypedInteger.h"
 #include "gtest/gtest.h"
 
 namespace dawn {
 namespace {
 
 // Tests for RangesOverlap
-TEST(Numeric, angesOverlap) {
+TEST(NumericTest, RangesOverlap) {
     // Range contains only one number
     ASSERT_EQ(true, RangesOverlap(0, 0, 0, 0));
     ASSERT_EQ(false, RangesOverlap(0, 0, 1, 1));
@@ -72,6 +75,45 @@ TEST(Numeric, angesOverlap) {
     // Negative numbers
     ASSERT_EQ(true, RangesOverlap(-9, 12, 4, 16));
     ASSERT_EQ(false, RangesOverlap(-9, -3, -2, 0));
+}
+
+// Death tests
+// Name "*DeathTest" per https://google.github.io/googletest/advanced.html#death-test-naming
+
+// Test for checked cast between various types.
+template <typename T32, typename T64>
+void CheckedCastTest() {
+    using I32 = UnderlyingType<T32>;
+    using I64 = UnderlyingType<T64>;
+    static_assert(std::is_same_v<I32, uint32_t>);
+    static_assert(std::is_same_v<I64, uint64_t>);
+
+    // No-ops
+    checked_cast<T32>(T32{std::numeric_limits<I32>::max()});
+    checked_cast<T64>(T64{std::numeric_limits<I64>::max()});
+
+    // Widening
+    checked_cast<T64>(T32{std::numeric_limits<I32>::max()});
+
+    // Narrowing
+    EXPECT_DEATH(checked_cast<T32>(T64{uint64_t{std::numeric_limits<I32>::max()} + 1}), "");
+    EXPECT_DEATH(checked_cast<T32>(T64{uint64_t{std::numeric_limits<I32>::max()} * 2}), "");
+    EXPECT_DEATH(checked_cast<T32>(T64{std::numeric_limits<uint64_t>::max()}), "");
+}
+TEST(NumericDeathTest, CheckedCast) {
+    CheckedCastTest<uint32_t, uint64_t>();
+
+    using TypedU32 = TypedInteger<struct TypedU32T, uint32_t>;
+    using TypedU64 = TypedInteger<struct TypedU64T, uint64_t>;
+    CheckedCastTest<uint32_t, TypedU64>();
+    CheckedCastTest<TypedU32, uint64_t>();
+    CheckedCastTest<TypedU32, TypedU64>();
+
+    enum class EnumU32 : uint32_t {};
+    enum class EnumU64 : uint64_t {};
+    CheckedCastTest<uint32_t, EnumU64>();
+    CheckedCastTest<EnumU32, uint64_t>();
+    CheckedCastTest<EnumU32, EnumU64>();
 }
 
 }  // anonymous namespace
