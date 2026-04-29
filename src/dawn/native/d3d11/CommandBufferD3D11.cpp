579447cf71643bde5652e5bd5e81eb55538e1ba0
Francisco Chavez <frankchavez@google.com>
2026-04-29 16:46:51 -0700

    spanification: Add #pragma allow_unsafe_buffers to Dawn (Linux/Win)
    As part of the effort to enforce -Wunsafe-buffer-usage globally in
    Dawn, this CL transitions from coarse, GN-level directory
    suppressions to targeted file-level `#pragma allow_unsafe_buffers`
    exclusions for currently failing files on Linux and Windows.
    
    This establishes a new baseline where newly added files, and
    existing files without the pragma, are strictly checked for safe
    buffer usage. This immediately prevents the introduction of new
    unsafe patterns into the codebase from clean files.
    
    Upcoming follow-ups for this effort include:
    - Running the automation script for additional platforms.
    - Removing the top-level directory exclusions from
      unsafe_buffers_paths.txt.
    - Transitioning from coarse file-level pragmas to fine-grained,
      instruction-level opt-outs (UNSAFE_TODO).
    
    This CL has no behavior changes.
    
    This patch was fully automated using:
    https://paste.googleplex.com/6662342478790656
    
    For more context, see the internal doc:
    http://go/unsafe-buffer-dawn
    
    Bug: 507078598, 504659032
    Change-Id: Ic233853bd3a8a8a4730c800cef25606c17639593
    Reviewed-on: https://dawn-review.googlesource.com/c/dawn/+/305577
    Commit-Queue: Francisco Ochoa <frankchavez@google.com>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Corentin Wallez <cwallez@chromium.org>
    SLSA-Policy-Verified: SLSA Policy Verification Service <devtools-gerritcodereview-exitgate@google.com>

--- src/dawn/native/d3d11/CommandBufferD3D11.cpp
+++ src/dawn/native/d3d11/CommandBufferD3D11.cpp
@@ -25,6 +25,11 @@
 // OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+#ifdef UNSAFE_BUFFERS_BUILD
+// TODO(crbug.com/40285824): Remove this and convert code to safer constructs.
+#pragma allow_unsafe_buffers
+#endif
+
 #include "dawn/native/d3d11/CommandBufferD3D11.h"
 
 #include <algorithm>
